981adebd588bb588f2b4af94dc8729926f0a2443
Joshua Wessels <joshuawessels04@gmail.com>
2018-05-30 13:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  return string.toUpperCase()
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  
 }
